e8d17cb0bb0c500447554b53deffcdec7c76bea5
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-03-01 14:12:03 +1300

    Add project files.

---
+++ xamlcs-howick-res-d/Helper/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace xamlcs_howick_res_d.Helper
+{
+    //conv isLess
+    public class IsLessThanConverter : IValueConverter
+    {
+        public static readonly IsLessThanConverter Instance = new IsLessThanConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double doubleValue = System.Convert.ToDouble(value);
+            double compareToValue = System.Convert.ToDouble(parameter);
+
+            return doubleValue < compareToValue ? doubleValue : compareToValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    // conv isGreater
+
+    public class IsGreaterThanConverter : IValueConverter
+    {
+        public static readonly IsLessThanConverter Instance = new IsGreaterThanConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double doubleValue = System.Convert.ToDouble(value);
+            double compareToValue = System.Convert.ToDouble(parameter);
+
+            return doubleValue > compareToValue; 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
